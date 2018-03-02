9dd65aaf268e3972736f65a21457de4da20ab84d
Coulton7 <matthew.coulton@aesseal.co.uk>
2018-03-02 14:40:06 +0000

    Adjusting the paths on php

--- template.php
+++ template.php
@@ -9,7 +9,7 @@ function aesseal_classic_preprocess_node(&$variables) {
 
 
 function hook_preprocess_page(&$vars) {
-  drupal_add_js('<?php echo base_path(); ?>sites/all/themes/aesseal_classic/js/hubspot-progressive-profile.js', 'external');
+  drupal_add_js('../js/hubspot-progressive-profile.js', 'external');
 }
 
 
@@ -32,4 +32,4 @@ function THEMENAME_preprocess_node(&$variables, $hook) {
  * @file template.php
  */
 
-drupal_add_js('sites/all/themes/aesseal_classic/js/contactustext.js');
+drupal_add_js('../js/contactustext.js');
